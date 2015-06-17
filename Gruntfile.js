c6a86f1c4e7de03c6ad0279ad3f8c08523ccc427
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-06-18 01:36:26 +0200

    Register an element using Polymer

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          keepalive: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default', []);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # polymer-hello-world
 polymer-hello-world
+
+# get dependencies
+`bower install`
+
+# Demo
+## get demo dependencies
+`npm install -d`
+
+## run the demo
+`grunt connect`
